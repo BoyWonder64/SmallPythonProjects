2f93b4bd5effa2728bdcfd90d41e1871bb571738
BoyWonder64 <maitland.andrus@gmail.com>
2026-03-06 19:43:15 -0700

    Project 5: Bouncing DVD Logo - Part 2 of writing logo. Established constants and wrote logic to determine if a corner is touched

--- Project5/bounceingDVDLogo.py
+++ Project5/bounceingDVDLogo.py
@@ -17,3 +17,63 @@ except ImportError:
     
 # Establish Constants
 WIDTH, HEIGHT = bext.size()
+
+# We can't print to the last column on Windows without it adding a
+# newline automatically, so reduce the width by one:
+WIDTH -= 1
+
+NUMBER_OF_LOGOS = 5 # (!) Feel free to change this to any value from 1 to 100
+PAUSE_AMOUNT = 0.2 # (!) Feel free to change this is any value like 1.0 or 0.0
+# (!) Try changing this list to fewer colors if you want
+COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+
+UP_RIGHT   = 'ur'
+UP_LEFT    = 'ul'
+DOWN_RIGHT = 'dr'
+DOWN_LEFT  = 'dl'
+DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+
+# Key names for logo dictionaries
+COLOR = 'color'
+X = 'x'
+Y = 'y'
+DIR = 'direction'
+
+def main():
+    bext.clear()
+
+    # Generate some logos.
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+        logos.append({COLOR: random.choice(COLORS), 
+                     X: random.randint(1, WIDTH - 4),
+                     Y: random.randint(1, HEIGHT - 4),
+                     DIR: random.choice(DIRECTIONS)})
+        if logos[-1][X] % 2 == 1:
+            # Make sure X is even so it can hit the corner
+            logos[-1][X] -= 1
+        
+        cornerBounces = 0 # Track how many times the logo hits a corner
+        while True:
+            # Main Loop beings here.
+            for logo in logos: # Handle each logo in the logos list
+                # Erase the logo's current location:
+                bext.goto(logo[X], logo[Y])
+                print('   ', end='') # (!) Try commenting this line out for funsies
+
+                originalDirection = logo[DIR]
+
+                # See if the logo bounces off the corners:
+                if logo[X] == 0 and logo[Y] == 0:
+                    logo[DIR] = DOWN_RIGHT
+                    cornerBounces += 1
+                elif logo[X] == 0 and logo[Y] == HEIGHT -1:
+                    cornerBounces += 1
+                elif logo[X] == WIDTH -3 and logo[Y] == 0:
+                    cornerBounces += 1
+                elif logo[X] == WIDTH -3 and logo[Y] == HEIGHT -1:
+                    logo[DIR] = UP_LEFT
+                    cornerBounces += 1
+
+                # See if the logo bounces off the left edge:
+                
